421c59cb68aa4df2bc47555835079006425ff342
島野雅子 <b25119ms@aiit.ac.jp>
2025-05-18 00:45:08 +0000

    コマンド分割

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,10 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:schema:load
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 
+bundle exec rails db:drop
+bundle exec rails db:create
+bundle exec rails db:schema:load
 bundle exec rails db:seed
+bundle exec rails db:migrate
