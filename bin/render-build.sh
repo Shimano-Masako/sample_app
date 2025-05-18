9570b08159e04dafdc28bd67e4b88af1940c9f84
島野雅子 <b25119ms@aiit.ac.jp>
2025-05-18 01:07:29 +0000

    del drop and create

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,8 +5,6 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop:_unsafe
-bundle exec rails db:create
 bundle exec rails db:schema:load
 bundle exec rails db:seed
 bundle exec rails db:migrate
--- config/initializers/monkey_patch_activerecord.rb
+++
@@ -1,6 +0,0 @@
-class ActiveRecord::Tasks::PostgreSQLDatabaseTasks
-  def drop
-    establish_master_connection
-    connection.execute "DROP DATABASE IF EXISTS \"#{db_config.database}\" WITH (FORCE)"
-  end
-end
