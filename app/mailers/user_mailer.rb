594d6f312c7988f9a7332efd164fe488d91270d2
島野雅子 <b25119ms@aiit.ac.jp>
2025-05-24 10:49:06 +0000

    add logger

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,13 @@
 class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
+
+    log = Logger.new(STDOUT)
+    log.fatal('==================================')
+    log.fatal(user.name)
+    log.fatal(user.email)
+    log.fatal('==================================')
+
     mail to: user.email, subject: 'Account activation'
   end
 
